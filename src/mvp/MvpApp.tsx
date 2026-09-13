4a255b4e3c1713bc1349784f715491d88989b3d6
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2026-09-12 23:51:06 -0300

    style(uiux): load approved dark premium theme

--- src/mvp/MvpApp.tsx
+++ src/mvp/MvpApp.tsx
@@ -11,6 +11,7 @@ import'./service-history.css'
 import'./stitch-client-provider-alignment.css'
 import'./request-evidence.css'
 import'./ugo-uiux-p0.css'
+import'./ugo-dark-premium.css'
 
 const AdminGate=lazy(()=>import('./AdminGate').then(module=>({default:module.AdminGate})))
 const ClientRoot=lazy(()=>import('./client/ClientRoot').then(module=>({default:module.ClientRoot})))
