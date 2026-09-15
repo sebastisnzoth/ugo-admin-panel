6c8b0c004d0bb687cc60b15de6286727771bbd6d
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2026-09-15 00:09:22 -0300

    style(client-ui): load Stitch live tracking overrides

--- src/mvp/client/ClientRoot.tsx
+++ src/mvp/client/ClientRoot.tsx
@@ -29,6 +29,7 @@ import'./client-guided-request-review.css'
 import'./client-redesign-2026.css'
 import'./client-stitch-orb-home.css'
 import'./client-stitch-radar.css'
+import'./client-stitch-tracking.css'
 
 type Props={demo:boolean}
 
