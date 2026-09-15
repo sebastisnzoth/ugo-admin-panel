61e96d283585504499a91c0ec02091802421733e
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2026-09-15 00:02:48 -0300

    style(client-ui): load Stitch orb home overrides

--- src/mvp/client/ClientRoot.tsx
+++ src/mvp/client/ClientRoot.tsx
@@ -27,6 +27,7 @@ import'./client-visual-polish.css'
 import'./client-guided-request-redesign.css'
 import'./client-guided-request-review.css'
 import'./client-redesign-2026.css'
+import'./client-stitch-orb-home.css'
 
 type Props={demo:boolean}
 
