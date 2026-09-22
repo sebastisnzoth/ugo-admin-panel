f0d8b609af26153ecfc9b921bbc54808aa4bbe52
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2026-09-22 11:56:44 -0300

    refactor(client): extract operational surfaces composition

---
+++ src/features/client/ui/ClientOperationalSurfaces.tsx
@@ -0,0 +1,13 @@
+import{AppLocationButton}from'../../../mvp/AppLocationButton'
+import{DisputeDock}from'../../../mvp/DisputeDock'
+import{ClientCompletionReview}from'../../../mvp/ClientCompletionReview'
+import{ClientLiveTracking}from'../../../mvp/ClientLiveTracking'
+import{ServiceChat}from'../../../mvp/ServiceChat'
+import{ClientPaymentChoice}from'../../../mvp/client/ClientPaymentChoice'
+import{ClientProviderRadarBridge}from'../../../mvp/client/ClientProviderRadarBridge'
+import{ClientRatingPrompt}from'../../../mvp/client/ClientRatingPrompt'
+
+type Props={canonical:boolean;detailOpen:boolean;screen:string;openDispute:()=>void}
+export function ClientOperationalSurfaces({canonical,detailOpen,screen,openDispute}:Props){
+ return <>{!canonical&&!detailOpen&&<ClientPaymentChoice/>}{!canonical&&!detailOpen&&<ClientLiveTracking/>}{!canonical&&!detailOpen&&<ClientCompletionReview onOpenDispute={openDispute}/>}{screen!=='request'&&!detailOpen&&<ClientRatingPrompt/>}{!canonical&&!detailOpen&&<ServiceChat role="client"/>}{!canonical&&!detailOpen&&<DisputeDock role="client" openRequest={screen==='dispute'}/>}{!canonical&&!detailOpen&&<AppLocationButton role="client"/>}{!canonical&&<ClientProviderRadarBridge/>}</>
+}
