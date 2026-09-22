3efebf1207f7472beff9cd1cd0a8207afa1abf6e
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2026-09-22 03:00:02 -0300

    Create base feature registry entries to organize app modules by domain

---
+++ src/features/index.ts
@@ -0,0 +1,24 @@
+export const clientFeature = {
+  name: 'client',
+  routes: ['client', 'client-web', 'web-client'],
+}
+
+export const providerFeature = {
+  name: 'provider',
+  routes: ['provider', 'provider-web', 'web-provider'],
+}
+
+export const adminFeature = {
+  name: 'admin',
+  routes: ['admin'],
+}
+
+export const financeFeature = {
+  name: 'finance',
+  routes: ['finance'],
+}
+
+export const operationsFeature = {
+  name: 'operations',
+  routes: ['operations', 'development'],
+}
