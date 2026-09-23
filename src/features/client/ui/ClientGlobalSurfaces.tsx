f74deed3d054edf5c8413d32b9647ccecb6a282a
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2026-09-23 01:32:28 -0300

    refactor(client): move Hugo bridge into feature boundary

--- src/features/client/ui/ClientGlobalSurfaces.tsx
+++ src/features/client/ui/ClientGlobalSurfaces.tsx
@@ -1,6 +1,6 @@
 import{ClientGlobalMenu}from'../../../mvp/ClientGlobalMenu'
 import{NotificationCenter,type UgoNotification}from'../../../mvp/NotificationCenter'
-import{ClientHugoBridge}from'../../../mvp/client/ClientHugoBridge'
+import{ClientHugoBridge}from'../hugo/ClientHugoBridge'
 
 type Props={onOpenNotice:(notice:UgoNotification)=>void}
 export function ClientGlobalSurfaces({onOpenNotice}:Props){
